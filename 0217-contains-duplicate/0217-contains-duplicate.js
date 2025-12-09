98711c293ddf6d1b75024e26c8786a82f21aeade
ashfak88 <ashfakkp323@gmail.com>
2025-12-09 13:18:09 +0530

    Time: 60 ms (12.63%), Space: 75.1 MB (29.62%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let unique =[...new Set(nums)].join("")
+   let num =nums.join("")
+   if(num==unique){
+    return false
+   } else{
+    return true
+   }
+   
+};
